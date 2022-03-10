bd2b4f5de3046289c8fa5cc4a1e73dafe1fcba43
QR <1826255833@qq.com>
2022-03-10 15:00:31 +0800

    rabbitmq 工具类

--- basic/src/main/java/org/qrl/mq/util/RabbitMqTool.java
+++ basic/src/main/java/org/qrl/mq/util/RabbitMqTool.java
@@ -1,9 +1,32 @@
 package org.qrl.mq.util;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 /**
  * @author qr
  * @date 2022/3/10 14:51
  */
 public class RabbitMqTool {
 
+    /**
+     * 初始化 ConnectionFactory
+     */
+    private final static ConnectionFactory factory = new ConnectionFactory();
+    static {
+        factory.setHost("120.77.16.74");
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+    }
+
+    /**
+     * 建立信道 channel
+     * @return channel
+     * @throws Exception 反正就是连接失败了。。。
+     */
+    public static Channel getChannel() throws Exception {
+        Connection connection = factory.newConnection();
+        return connection.createChannel();
+    }
 }
